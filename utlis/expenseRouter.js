776f6a0cf4d4c1bb90bdc44468df91285d195d73
Rohit Kumar <rohitt24k@gmail.com>
2024-04-03 15:45:40 +0530

    fixed error for dialyAverageSpend

--- utlis/expenseRouter.js
+++ utlis/expenseRouter.js
@@ -46,17 +46,7 @@ async function queryOnData(data, date) {
     0
   );
 
-  function getNumberOfDaysInMonth(dateString) {
-    // Split the date string into year and month parts
-    const [year, month] = dateString.split("-");
-    // Create a new Date object for the first day of the next month
-    // (day 0 of the next month will be the last day of the current month)
-    const lastDayOfMonth = new Date(year, month, 0).getDate();
-    // Return the number of days in the month
-    return lastDayOfMonth;
-  }
-
-  const totalDaysInMonth = getNumberOfDaysInMonth(date);
+  const totalDaysInMonth = Number(date.split("-")[2]);
   const dailyAverageSpendCurrentMonth =
     totalSpendCurrentMonth / totalDaysInMonth;
 
